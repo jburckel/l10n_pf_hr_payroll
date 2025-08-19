d6aae273dbed64a7a2f9a5d6c3673b13ae89851f
jburckel <jburckel@natimai.solutions>
2025-08-19 11:42:04 -1000

    Remove hr_payslip_worked_days.py

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,6 @@ from . import hr_employee
 from . import hr_payslip
 from . import hr_payslip_input
 from . import hr_payslip_line
-from . import hr_payslip_worked_days
 from . import hr_salary_rule
 from . import res_config_settings
 
